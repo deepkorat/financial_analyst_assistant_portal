0ffbf44101ea98d716791683c0672459b0b73e87
Deep Korat <deepkorat13@gmail.com>
2024-10-21 18:19:14 +0530

    dashboard gpt

--- app/routes.py
+++ app/routes.py
@@ -8,13 +8,15 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    # return "Welcome to the Financial Analyst Assistant Portal!"
     return render_template('index.html')
+
+@main.route('/main')
+def my_main():
+    return render_template('main.html')
 
 @main.route('/upload_page')
 def upload_page():
     return render_template('base.html')
-
 
 @main.route('/upload', methods=['POST'])
 def upload():
@@ -25,13 +27,11 @@ def upload():
         file = request.files['annual_report']
         if file.filename == '':
             return jsonify({"error": "No selected file"})
-            # return "Error: No selected file"
 
         file_path = os.path.join('uploads', file.filename)
         file.save(file_path)
 
         return render_template('dashboard.html', filename = file.filename )
-
 
     # return render_template('main.html', result = result)
 
@@ -41,35 +41,42 @@ def ai_assist():
     # return render_template('ai-assist.html')
     return render_template('dashboard.html')
 
+@main.route('/gpt_dashboard')
+def gpt_dashboard():
+    return render_template('gpt_dashboard.html')
 
 @main.route('/ai', methods=['POST'])
 def ai_assist_form():
     query = request.form['query']
-    answer="This is my annual report answer"
+    answer="This is my annual report answer" ## implement your model here and fetch answer
     return render_template('ai-assist.html', query= query, answer=answer)
-
 
 @main.route('/dashboard2')
 def dashboard2():
     return render_template('dashboard2.html')
-
-
 
 @main.route('/data')
 def send_data():
     data = {'key': 'value'}
     return jsonify(data)
 
+
+
+    
 @main.route('/ask', methods=['POST'])
 def ask_question():
     # Get the question from the request (sent via JavaScript)
     question = request.json.get('question')
     
+
     # Example simple model logic for answering
-    response = "this is your anawer"
+    response = "this is my flask answer."
     
     # Send the response back to JavaScript as JSON
-    return jsonify({'answer': response})
+    response_data = {"message": "Data received successfully", "response": response}
+
+    return jsonify(response_data)
+
 
 
 @main.route('/news')
